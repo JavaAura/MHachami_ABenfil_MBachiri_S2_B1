4cbd2eae37d519d2afbc6bc30260c4337da3b1e4
MedHachami <moha.hach.9@gmail.com>
2024-10-01 19:00:39 +0100

    member entity

--- src/main/java/entities/Member.java
+++ src/main/java/entities/Member.java
@@ -3,11 +3,72 @@ package entities;
 import enums.Role;
 
 public class Member {
-	private Long id;
+	private Long Id;
 	private String firstName;
-	private String secondname;
+	private String secondName;
 	private String email;
 	private Role userRole;
 
+
+	public Member(){}
+
+	public Member(String firstName , String secondName,String email , Role usRole){
+		this.firstName = firstName;
+		this.secondName = secondName;
+		this.email = email;
+		this.userRole = userRole;
+	}	
+
+	public Member(Long Id,String firstName , String secondName,String email , Role usRole){
+		this.Id = Id;
+		this.firstName = firstName;
+		this.secondName = secondName;
+		this.email = email;
+		this.userRole = userRole;
+
+	}
+
+
+	public Long getId() {
+        return this.Id;
+    }
+
+    public void setId(Long Id) {
+        this.Id = Id;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return this.secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Role getUserRole() {
+        return this.userRole;
+    }
+
+    public void setUserRole(Role userRole) {
+        this.userRole = userRole;
+    }
+
+	
 	
 }
