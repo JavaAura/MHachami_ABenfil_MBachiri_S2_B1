a24cbaedb8c03d3f4948be3a936ee0f061eb61c3
MedHachami <moha.hach.9@gmail.com>
2024-10-01 14:36:48 +0100

    db connection and packages for business logic

---
+++ src/main/java/entities/Member.java
@@ -0,0 +1,13 @@
+package entities;
+
+import enums.Role;
+
+public class Member {
+	private Long id;
+	private String firstName;
+	private String secondname;
+	private String email;
+	private Role userRole;
+
+	
+}
