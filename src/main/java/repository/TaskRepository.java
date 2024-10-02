1e7764b270b9e586ae20b175f9ab78add2303290
Mohammed El Bachiri <mohammed-el-bachiri@users.noreply.github.com>
2024-10-02 09:55:43 +0100

    create task repository interface

---
+++ src/main/java/repository/TaskRepository.java
@@ -0,0 +1,13 @@
+package repository;
+
+import entities.Task;
+import java.sql.SQLException;
+import java.util.List;
+
+public interface TaskRepository {
+    void addTask(Task task) throws SQLException;
+    List<Task> getAllTasks() throws SQLException;
+    Task getTaskById(int id) throws SQLException;
+    void updateTask(Task task) throws SQLException;
+    void deleteTaskById(int id) throws SQLException;
+}
