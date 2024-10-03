c8c241095f4646c37f0232e865caa48bf0e4c21b
Anass Benfillous <anasse.benfillous@gmail.com>
2024-10-03 12:13:59 +0100

    use sevice layer to manage logic and input validation

--- src/main/java/service/Impl/ProjectServiceImpl.java
+++ src/main/java/service/Impl/ProjectServiceImpl.java
@@ -1,46 +1,99 @@
-//package service.Impl;
-//
-//import java.sql.SQLException;
-//import java.util.HashMap;
-//import java.util.List;
-//
-//import entities.Project;
-//import service.ProjectService;
-//
-//public class ProjectServiceImpl implements ProjectService {
-//
-//	@Override
-//	public Project getProject(int id) throws SQLException {
-//		return repository.readById(id);
-//	}
-//
-//	@Override
-//	public List<Project> getAllProjects() throws SQLException {
-//		return repository.read();
-//	}
-//
-//	@Override
-//	public Project createProject(Project project) throws SQLException {
-//		return repository.create(project);
-//	}
-//
-//	@Override
-//	public Project updateProject(Project project) throws SQLException {
-//		return repository.update(project);
-//	}
-//
-//	@Override
-//	public void deleteProject(Project project) throws SQLException {
-//		repository.delete(project);
-//	}
-//
-//	@Override
-//	public List<Project> searchForProject(String title) throws SQLException {
-//		return repository.searchByTitle(title);
-//	}
-//
-//	@Override
-//	public HashMap<Integer, Integer> getProjectStats(int id) {
-//		return null;
-//	}
-//}
+package service.Impl;
+
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+
+import entities.Project;
+import enums.ProjectStatus;
+import repository.Impl.ProjectRepositoryImpl;
+import service.ProjectService;
+
+public class ProjectServiceImpl implements ProjectService {
+
+	private ProjectRepositoryImpl repository = new ProjectRepositoryImpl();
+
+	@Override
+	public Project getProject(String id) {
+		if (id == null)
+			return null;
+
+		try {
+			return repository.readById(Integer.parseInt(id));
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	@Override
+	public List<Project> getAllProjects() {
+		List<Project> projects = null;
+		try {
+			projects = repository.read();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return projects;
+	}
+
+	@Override
+	public Project createProject(String name, String description, LocalDate startDate, LocalDate endDate) {
+		Project project = new Project(name, description, startDate, endDate, new Project().getStatus());
+		try {
+			repository.create(project);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return project;
+	}
+
+	@Override
+	public Project updateProject(String name, String description, LocalDate startDate, LocalDate endDate, String status,
+			int id) {
+		ProjectStatus projectStatus = ProjectStatus.valueOf(status);
+		Project project = new Project(name, description, startDate, endDate, projectStatus);
+		project.setId(id);
+		try {
+			repository.update(project);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return project;
+	}
+
+	@Override
+	public Project deleteProject(String id) {
+		if (id == null)
+			return null;
+		Project project = null;
+		int projectId = Integer.parseInt(id);
+		try {
+			project = repository.readById(projectId);
+			if (project == null)
+				return null;
+			repository.delete(project);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return project;
+	}
+
+	@Override
+	public List<Project> searchForProject(String title) {
+
+		List<Project> projects = null;
+		try {
+			projects = repository.searchByTitle(title);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return projects;
+	}
+
+	@Override
+	public HashMap<Integer, Integer> getProjectStats(int id) {
+		return null;
+	}
+}
